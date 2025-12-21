cd665f3ccd8437264ff7fc0525178c1b16b83290
AmirabbsShahnavaz <amirabbas.shahnavaz@gmail.com>
2025-12-21 20:21:03 +0330

    Change admin email and password for superuser creation

--- create_admin.py
+++ create_admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "yourpassword")
+    User.objects.create_superuser("admin", "amirabbas.shahnavaz@gmail.com", "123")
     print("Superuser created!")
 else:
     print("Admin already exists.")
